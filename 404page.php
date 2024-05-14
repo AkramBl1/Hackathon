e4db820151a9da43e5f28af251307d7347427846
Akram Ibnelyazyd <155922765+AkramBl1@users.noreply.github.com>
2024-05-14 17:51:45 +0100

    Create 404page.php

---
+++ 404page.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>404 Not Found</title>
+  <link rel="stylesheet" href="css/404.css">
+  <style>
+	.sign-up{
+	margin: 3rem auto;
+	display: block;
+
+	width: 	10rem;
+	height: 3rem;
+	border-radius: 10px;
+
+	& a{
+		color: black;
+		text-decoration: none;
+		font-size: 1rem;
+		font-weight: bold;
+		cursor: pointer;
+	}
+	}
+  </style>
+</head>
+<body>
+<figure>
+	<div class="sad-mac"></div>
+	<figcaption>
+		<span class="sr-text">Error 404: Not Found</span>
+		<span class="e"></span>
+		<span class="r"></span>
+		<span class="r"></span>
+		<span class="o"></span>
+		<span class="r"></span>
+		<span class="_4"></span>
+		<span class="_0"></span>
+		<span class="_4"></span>
+		<span class="n"></span>
+		<span class="o"></span>
+		<span class="t"></span>
+		<span class="f"></span>
+		<span class="o"></span>
+		<span class="u"></span>
+		<span class="n"></span>
+		<span class="d"></span>
+	</figcaption>
+<button class="sign-up"> <a href="index.php">Sign up</a></button>
+
+</figure>
+
+  </div>
+</body>
+</html>
